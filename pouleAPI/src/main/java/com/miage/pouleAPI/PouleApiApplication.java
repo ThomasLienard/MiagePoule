67a2d189cc946704c5c69b43785038847a3b554f
BaptisteParent <baptiste.parent.etu@univ-lille.fr>
2025-09-24 20:55:55 +0200

    init spring boot

---
+++ pouleAPI/src/main/java/com/miage/pouleAPI/PouleApiApplication.java
@@ -0,0 +1,13 @@
+package com.miage.pouleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PouleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PouleApiApplication.class, args);
+	}
+
+}
---
+++ pouleAPI/src/test/java/com/miage/pouleAPI/PouleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.pouleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PouleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
