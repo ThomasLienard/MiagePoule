558c6bd69b5af2ed745076d208d6df6cf2c99560
maureencfr <maureen.coffre@gmail.com>
2025-11-19 14:25:15 +0100

    front d'affichage des compétitions(sans épreuves pour le moment), en attente de la BDD pour se baser sur le backk

---
+++ front/src/components/Competition.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/Competition.css";
+
+const Competition = ({ competition }) => {
+    const { nom, dateDebut, dateFin, lieu, description } = competition;
+    return (
+        <div className="competition">
+            <h3>{nom}</h3>
+            <h4> {description}</h4>
+            <p><strong>Date de début:</strong> {formatDate(dateDebut)}</p>
+            <p><strong>Date de fin:</strong> {formatDate(dateFin)}</p>
+            <p><strong>Lieu:</strong> {lieu}</p>
+        </div>
+    );
+};
+
+function formatDate(dateStr) {
+    const [year, month, day] = dateStr.split("-");
+    return `${day}/${month}/${year}`;
+}
+
+
+export default Competition;
