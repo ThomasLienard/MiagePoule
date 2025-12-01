6d6703e7ccc64ede1e42c51a28e038e87b4e5377
Coffre Maureen <maureen.coffre.etu@b12p24.fil.univ-lille.fr>
2025-12-01 12:16:04 +0100

    code des services

---
+++ KernelService/src/main/java/com/miage/pouleAPI/services/impl/CompetitionServiceImpl.java
@@ -0,0 +1,33 @@
+package com.miage.pouleAPI.services.impl;
+
+import com.miage.pouleAPI.domains.CompetitionModel;
+import com.miage.pouleAPI.domains.ports.CompetitionPort;
+import com.miage.pouleAPI.services.CompetitionService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CompetitionServiceImpl implements CompetitionService {
+
+    private CompetitionPort competitionPort;
+
+    public CompetitionServiceImpl(CompetitionPort competitionPort) {
+        this.competitionPort = competitionPort;
+    }
+
+
+    @Override
+    public List<CompetitionModel> findAll() {
+        return competitionPort.findAll();
+    }
+
+    @Override
+    public Optional<CompetitionModel> findById(Integer id) {
+        return competitionPort.findById(id);
+    }
+
+    @Override
+    public CompetitionModel save(CompetitionModel competition) {
+        return competitionPort.save(competition);
+    }
+}
