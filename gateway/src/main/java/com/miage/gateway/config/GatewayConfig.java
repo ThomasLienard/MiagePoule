12a5f0968e6c31d9740950832033f611c852d35e
maureencfr <maureen.coffre@gmail.com>
2025-12-08 13:40:12 +0100

    Add GET endpoints for championship competitions

--- gateway/src/main/java/com/miage/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/miage/gateway/config/GatewayConfig.java
@@ -74,6 +74,8 @@ public class GatewayConfig {
                                 .POST("/championship/{champId}/events/create", http())
                                 .PUT("/championship/{champId}/events/{eventId}/update", http())
                                 .DELETE("/championship/{champId}/events/{eventId}/delete", http())
+                                .GET("/public/championship/{champId}/comp/{compId}", http())
+                                .GET("/public/championship/{champId}/comp/{compId}/events", http())
                                 .GET("/public/championship/{champId}/comp/{compId}/events/{eventId}", http())
 
                                 .GET("/public/championship/{champId}/events/{eventId}/athletes", http())
