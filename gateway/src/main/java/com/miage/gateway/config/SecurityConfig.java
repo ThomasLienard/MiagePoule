2eef52646099503ec995fbc6fb6f52195b2689c2
baptiste.parent.etu <baptiste.parent.etu@univ-lille.fr>
2025-12-15 15:18:47 +0100

    add cors config

--- gateway/src/main/java/com/miage/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/miage/gateway/config/SecurityConfig.java
@@ -4,7 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -12,18 +19,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
-                )
-            .csrf(csrf -> csrf.disable()); // souvent désactivé pour les APIs
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                );
 
-
-        // Provide both HTTP Basic (browser prompt) and a simple form login page
         http.httpBasic(withDefaults());
-        http.formLogin(withDefaults());
 
         return http.build();
     }
