f5f8fdd4a183178bb37409fc1fd98fd9b8ea2c5b
maureencfr <maureen.coffre@gmail.com>
2026-02-02 17:17:08 +0100

    test exclusion sonarcloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',  // pour Docker
+    baseUrl: 'http://localhost:3001',
     chromeWebSecurity: false
   },
 });
