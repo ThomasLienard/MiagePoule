1a4f18f939aeca163b17297f2799b4831bbf1f29
ThomasLienard <thomas.lienard2@gmail.com>
2026-02-02 15:02:35 +0100

    feat: New endpoint get trials assigned to a user

--- KernelService/src/main/java/com/miage/pouleAPI/repositories/TrialRepository.java
+++ KernelService/src/main/java/com/miage/pouleAPI/repositories/TrialRepository.java
@@ -13,5 +13,19 @@ import java.util.List;
 public interface TrialRepository extends JpaRepository<Trial, Integer> {
     @Query("SELECT t FROM Trial t WHERE t.competition.id = :competitionId")
     List<Trial> findByCompetitionId(@Param("competitionId") Integer competitionId);
+    
+    @Query("SELECT t FROM Trial t " +
+            "JOIN IsConvenedTo i ON t.id = i.trial.id " +
+            "WHERE i.user.id = :userId " +
+            "ORDER BY t.timeSlot.start")
+    List<Trial> findSoloTrialsByUserId(@Param("userId") Integer userId);
+
+    @Query("SELECT t FROM Trial t " +
+            "JOIN ParticipateAt p ON t.id = p.trial.id " +
+            "JOIN Team team ON p.team.id = team.id " +
+            "JOIN ApplicationUser u ON u MEMBER OF team.users " +
+            "WHERE u.id = :userId " +
+            "ORDER BY t.timeSlot.start")
+    List<Trial> findTeamTrialsByUserId(@Param("userId") Integer userId);
 }
 
