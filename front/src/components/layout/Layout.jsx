e620db3e8dd595cf772acbe579015256f06f13fc
baptiste.parent.etu <baptiste.parent.etu@univ-lille.fr>
2026-03-09 10:27:17 +0100

    update

--- front/src/components/layout/Layout.jsx
+++ front/src/components/layout/Layout.jsx
@@ -19,12 +19,6 @@ export default function Layout() {
         }
     };
 
-    const handleNotificationClick = (eventId) => {
-        if (eventId) {
-            navigate(`/public/trials/${eventId}`);
-        }
-    };
-
     const popover = (
         <Popover id="popover-basic" className="notification-panel">
             <Popover.Header as="h4">Notifications</Popover.Header>
